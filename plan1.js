249fd95405276b5323b109839901c11eee810c8f
ahmedWa1408 <aa1229550@gmail.com>
2026-06-21 15:48:18 +0300

    Add plan1.js with location data and maps

---
+++ plan1.js
@@ -0,0 +1,214 @@
+const plan1=[
+
+{
+code:"QSMSM204-205",
+name:"ثلاث فلاشات",
+storage:"3 فلاشات",
+map:"https://maps.app.goo.gl/rD6qtCLzpYay22AN6?g_st=ipc"
+},
+
+{
+code:"113-114",
+name:"موقع 113-114",
+storage:"",
+map:"https://maps.app.goo.gl/ScMENYkk7k7s62KR7?g_st=iw"
+},
+
+{
+code:"139",
+name:"موقع 139",
+storage:"هاردسك",
+map:"https://www.google.com/maps/place/26%C2%B007'31.1%22N+43%C2%B057'32.8%22E/@26.1253188,43.9616809,652m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d26.125314!4d43.959106?entry=ttu"
+},
+
+{
+code:"100",
+name:"موقع 100",
+storage:"",
+map:"https://goo.gl/maps/FLZCuf1CWLq3DhHz5"
+},
+
+{
+code:"UMZSM194",
+name:"UMZSM194",
+storage:"",
+map:"https://maps.app.goo.gl/ucDGXCynoh2dbw1H9?g_st=iw"
+},
+
+{
+code:"QSMTE149-150",
+name:"QSMTE149-150",
+storage:"",
+map:"https://maps.app.goo.gl/XAutYAd5ZtNnaU43A?g_st=ic"
+},
+
+{
+code:"188-189",
+name:"188-189",
+storage:"",
+map:"https://maps.app.goo.gl/j9T84niBqVaWgSrC8?g_st=iw"
+},
+
+{
+code:"QSMIT149-150",
+name:"إشارة هاردسك",
+storage:"2 هاردسك",
+map:"https://maps.app.goo.gl/ui7hjoWeNq1VtUAz5?g_st=iw"
+},{
+code:"178-179",
+name:"موقع 178-179",
+storage:"",
+map:"https://maps.app.goo.gl/zvDwCJzyKpaD3cMx5?g_st=iw"
+},
+
+{
+code:"67-68",
+name:"موقع 67-68",
+storage:"",
+map:"https://goo.gl/maps/G33sQ4qWF3iL8jJt8"
+},
+
+{
+code:"QSMTE137-138",
+name:"QSMTE137-138",
+storage:"",
+map:"https://goo.gl/maps/Afaund5xT2LuUytt6"
+},
+
+{
+code:"176-177",
+name:"موقع 176-177",
+storage:"",
+map:"https://maps.app.goo.gl/k8qh4Nap8tQRKJFY6?g_st=iw"
+},
+
+{
+code:"255-256",
+name:"موقع 255-256",
+storage:"",
+map:"https://maps.app.goo.gl/htN3LhE4hQZg8q8k7?g_st=iw"
+},
+
+{
+code:"UNZSM197-198",
+name:"UNZSM197-198",
+storage:"",
+map:"https://maps.app.goo.gl/nqg5SNu4DyFSeDvU7?g_st=ipc"
+},
+
+{
+code:"QSMIT001",
+name:"انعطاف",
+storage:"",
+map:"https://maps.app.goo.gl/sezps3xvYsuCv5fs8?g_st=iw"
+},
+
+{
+code:"QSMIT021",
+name:"إشارة الساعة",
+storage:"هاردسك",
+map:"https://maps.app.goo.gl/FgL3dnSySX2woWi99?g_st=iw"
+},
+
+{
+code:"QSMIT006",
+name:"إشارة هاردسك",
+storage:"هاردسك",
+map:"https://goo.gl/maps/RWUR6vBNXvP6Rxg9A"
+},{
+code:"QSMIT033",
+name:"مقابل 144",
+storage:"",
+map:"https://maps.app.goo.gl/9GfrebVeNdhZFAEm6?g_st=iw"
+},
+
+{
+code:"QSMIT144",
+name:"انعطاف",
+storage:"",
+map:"https://maps.app.goo.gl/JLqtfxFDuQG3Cjdy9?g_st=iw"
+},
+
+{
+code:"143",
+name:"موقع 143",
+storage:"هاردسك",
+map:"https://maps.app.goo.gl/8B4kJev31yEiM9ef7?g_st=iw"
+},
+
+{
+code:"QSMSM328-329",
+name:"QSMSM328-329",
+storage:"",
+map:"https://maps.app.goo.gl/JHXSuswzQQ2FwFFa8?g_st=ipc"
+},
+
+{
+code:"QSMSM326-327",
+name:"QSMSM326-327",
+storage:"",
+map:"https://maps.app.goo.gl/5wa7NHV1EfEj3Msw9?g_st=ipc"
+},
+
+{
+code:"MTNSM003-004",
+name:"ثلاث فلاشات",
+storage:"3 فلاشات",
+map:"https://www.google.com/maps/place/25%C2%B055'54.4%22N+44%C2%B006'59.0%22E/@25.9316716,44.1169308,194m/data=!3m1!1e3!4m4!3m3!8m2!3d25.931778!4d44.116378?entry=ttu"
+},
+
+{
+code:"MTNCP001-002",
+name:"ثلاث فلاشات",
+storage:"3 فلاشات",
+map:"https://maps.app.goo.gl/tCaqrGE3bAbuWHoE8?g_st=iw"
+},
+
+{
+code:"MTNIT001",
+name:"إشارة هاردسك",
+storage:"هاردسك",
+map:"https://maps.app.goo.gl/PJCkL6CryS4hSknm7?g_st=com.google.maps.preview.copy"
+},{
+code:"QSMSM315-316",
+name:"QSMSM315-316",
+storage:"",
+map:"https://maps.app.goo.gl/rtXkWd1CFYaKCEk76?g_st=iw"
+},
+
+{
+code:"MTN001-002",
+name:"فلاشين",
+storage:"2 فلاش",
+map:"https://maps.app.goo.gl/WBGif5dRWEAUaMCD8?g_st=iw"
+},
+
+{
+code:"MTNIT002",
+name:"إشارة هاردسك - المذنب",
+storage:"هاردسك",
+map:"https://maps.app.goo.gl/Fdh5mLbHP9GyeyYA6?g_st=iw"
+},
+
+{
+code:"QSMSM223",
+name:"بعد المذنب",
+storage:"",
+map:"https://maps.app.goo.gl/kGoNJQzqfSigLPmF9?g_st=iw"
+},
+
+{
+code:"QSMSM226-227",
+name:"QSMSM226-227",
+storage:"",
+map:"https://maps.google.com?q=26.1398745,44.2108727&entry=gps"
+},
+
+{
+code:"QSMSM151",
+name:"QSMSM151",
+storage:"",
+map:"https://maps.app.goo.gl/XLTKDAwEiUeTNqFh8?g_st=iw"
+}
+
+];
